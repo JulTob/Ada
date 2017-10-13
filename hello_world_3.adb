99110b2615a96116e6b2d5595cd8f2197e14eb78
Julio <JulioToboso@gmail.com>
2017-10-13 18:44:15 +0200

    Create hello_world_3.adb
    package use!

---
+++ hello_world_3.adb
@@ -0,0 +1,9 @@
+with Ada.Text_IO;
+
+procedure Hello_World_3 is
+   use Ada.Text_IO;
+begin
+   Put_Line ("Hello World!");
+   New_Line;
+   Put_Line ("I am an Ada programm with package use.");
+end Hello_World_3;
