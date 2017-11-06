dfb744fda3a6d22e425901f1c112abe1f72bc31f
Julio <JulioToboso@gmail.com>
2017-11-06 14:17:54 +0100

    Create HelloAdaUse.adb

---
+++ HelloAdaUse.adb
@@ -0,0 +1,10 @@
+with Ada.Text IO; 
+
+use Ada.Text IO;  --You can use "use" to hide the precall to libraries
+
+procedure Hello is
+begin
+
+Put Line(”Hello, Ada!”); --Here you don need the "Ada.Text_IO."
+
+end Hello;
