672d2bad0b023aaab0eb95d5951932194952aa0f
Julio <JulioToboso@gmail.com>
2017-11-08 18:52:20 +0100

    Update Hello_Procedure.adb

--- Hello_Procedure.adb
+++ Hello_Procedure.adb
@@ -1,9 +1,18 @@
 --Non working example of a procedure syntaxis
 
 
-procedure Mode_Example (X : in Integer; Y : out Integer; Z : in out Integer) is
+procedure Mode_Example (X : in Integer:=0;    --Read only parameter with default value
+                        Y : out Integer;      --Write only var 
+                        Z : in out Integer) is
 begin
 X := 1; −− Error. Can’t modify an in parameter.
 Y := X; −− Okay. Can read X and modify Y.
 Z := Z + 1; −− Okay. Can read and write an in out parameter.
-end Mode Example;
+end Mode_Example;
+
+
+
+
+--Named association:
+
+Mode_Example(Z => Accumulator, X => N+15, Y => Output);
