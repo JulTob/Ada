8b01b65d70b16f8f14af434728ac36a5c2a2939c
Julio <JulioToboso@gmail.com>
2017-11-18 12:14:44 +0100

    Update hello_world.adb

--- hello_world.adb
+++ hello_world.adb
@@ -2,20 +2,19 @@ with Ada.Text_IO;		--Uso de la libreria de Texto In/Out
 
 procedure Hello_World is	--Declaracion del programa
 
-	package T_IO renames Ada.Text_IO;
-			--Sustituye con T_IO para llamar a la libreria Text_IO
+	   use Ada.Text_IO;	--SPermite llamar a la libreria Text_IO como si estuviera cargada
 
 
-begin 				--Comienza la definicion del programa
+begin 	--Hello_World		 --Comienza la definicion del programa
 
-	T_IO.Put_Line("Hola mundo");
+	Put_Line("Hola mundo");
 			--Las dobles comillas ("...") declaran un array de chars
 			--Put_Line es una función que imprime por salida estandar
 			
-	IO.New_Line;	--Goes a line down
+	New_Line;	--Goes a line down
 	
-   	IO.Put("I am an Ada programm with package rename."); 	--Writtes but doesnt start a new line
-	IO.New_Line;	--Goes a Line Down
+   	Put("I am an Ada programm with package rename."); 	--Writtes but doesnt start a new line
+	New_Line;	--Goes a Line Down
 	
 end Hello_World;
 
