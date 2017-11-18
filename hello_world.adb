81792768020d7131bc88fa048ab1cb63a4ad70c6
Julio <JulioToboso@gmail.com>
2017-11-18 12:11:57 +0100

    Update hello_world.adb

--- hello_world.adb
+++ hello_world.adb
@@ -11,7 +11,10 @@ begin 				--Comienza la definicion del programa
 	T_IO.Put_Line("Hola mundo");
 			--Las dobles comillas ("...") declaran un array de chars
 			--Put_Line es una función que imprime por salida estandar
-
+	IO.New_Line;
+   	IO.Put("I am an Ada programm with package rename.");
+	IO.New_Line;
+	
 end Hello_World;
 
 
