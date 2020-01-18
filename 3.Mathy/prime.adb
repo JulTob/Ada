7d3e26371a1fb0639787af364cd576346303b340
Julio <JulioToboso@gmail.com>
2020-01-18 02:22:07 +0100

    Update prime.adb

--- 3.Mathy/prime.adb
+++ 3.Mathy/prime.adb
@@ -3,6 +3,42 @@ with Ada.Integer Text IO; use Ada.Integer Text IO;
 
 procedure Prime is
 
+  function Is_Prime(N : Integer) return Boolean is
+  ---   O(N)
+  begin
+     for I in 2 .. (Number/2+1)loop
+      if Number rem I = 0 then
+        return false; 
+      end if;
+    end loop;
+    return true; 
+  end Is_Prime;
+  
+  
+  function Is_Prime2(N : Integer) return Boolean is
+    ---   O(sqrt(N))
+     test: positive := 2;
+     Upper : Positive := N-1;
+  begin
+    while test <= upper loop
+      pragma Loop_Invariant(Trial_Divisor <= N);
+      
+      if N rem test = 0 then
+        return false;
+      end if;
+      
+      upper := N/ test;
+      test := test + 1;
+    end loop;
+    
+    return true;
+    
+  end Is_Prime2;
+  
+  
+  
+  
+  
 Number : Integer;
 
 begin
@@ -17,12 +53,10 @@ if Number < 2 then
 else
   Put(”The value ”); 
   Put(Number, 0); 
-  for I in 2 .. (Number/2+1)loop
-    if Number rem I = 0 then
+  if Is_Prime(Number) then
       Put Line(” is not prime.”); 
-      return;
-    end if;
-  end loop;
-  Put Line(” is prime.”); 
+  else 
+    Put Line(” is prime.”); 
+  end if;
 end if;
 end Prime;
