2ec45040d997d211b21266414084a41125370147
Julio <JulioToboso@gmail.com>
2020-05-05 22:00:22 +0200

    Update tablas_de_multiplicar.adb

--- 3. Mathy/tablas_de_multiplicar.adb
+++ 3. Mathy/tablas_de_multiplicar.adb
@@ -1,22 +1,21 @@
 with Ada.Text_IO;
 
-	-- Programa que muestras las tablas de multiplicar
+-- Programa que muestras las tablas de multiplicar
 procedure Tablas_De_Multiplicar is
 	Resultado: Integer;
-
- begin
-	Ada.Text_IO.Put_Line("Tablas de Multiplicar");
-	Ada.Text_IO.Put_Line("=====================");
-	Ada.Text_IO.New_Line(2);
-	for Fila in 1..10 loop
-
+	begin
+		Ada.Text_IO.Put_Line("Tablas de Multiplicar");
+		Ada.Text_IO.Put_Line("=====================");
+		Ada.Text_IO.New_Line(2);
+		for Fila 	in 1..10 loop
 		for Columna in 1..10 loop
-			Resultado := Fila * Columna;
-			Ada.Text_IO.Put (Integer'Image(Fila) & " * " &
+				Resultado := Fila * Columna;
+				Ada.Text_IO.Put (
+					Integer'Image(Fila) & " * " &
 					Integer'Image(Columna) & " = " &
 					Integer'Image(Resultado));
+				Ada.Text_IO.New_Line;
+				end loop;
 			Ada.Text_IO.New_Line;
-		    end loop;
-		Ada.Text_IO.New_Line;
-	    end loop;
-    end Tablas_De_Multiplicar;
+			end loop;
+		end Tablas_De_Multiplicar;
