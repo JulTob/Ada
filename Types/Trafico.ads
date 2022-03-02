f510a94048ec0c12c4e46a52dbfa54437b2bf035
Julio <JulioToboso@gmail.com>
2022-03-02 17:23:15 +0100

    Create Trafico.ads

---
+++ Types/Trafico.ads
@@ -0,0 +1,22 @@
+
+with Ada.Text_IO;
+with Ada.Integer_Text_IO;
+
+
+package Trafico is
+
+	--Renames--
+	package T_IO renames Ada.Text_IO;
+    	package I_IO renames Ada.Integer_Text_IO;
+
+
+	--Types--
+	type Tipo_Trafico is (Lento, Moderado, Correcto, Rapido, Punible);
+	
+
+	--Functions & Procedures--
+	function Clasifica(Speed: integer) return Tipo_Trafico;
+
+	
+
+end Trafico;
