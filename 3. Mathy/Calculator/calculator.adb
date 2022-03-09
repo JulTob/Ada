d0e5d138da8b1768796e6121936f04208460160f
Julio <JulioToboso@gmail.com>
2022-03-09 01:00:41 +0100

    Create calculator.adb

---
+++ 3. Mathy/Calculator/calculator.adb
@@ -0,0 +1,27 @@
+with Ada.Text_IO, Ada.Integer_Text_IO;
+use  Ada.Text_IO, Ada.Integer_Text_IO;
+procedure Calculator is
+    First, Second : Integer;
+    Operator      : Character;
+    begin
+      Put ("Enter an expression: ");
+      Get (First);
+      Get (Operator);
+      Get (Second);
+      case Operator is
+        when '+' =>
+            Put (First + Second, Width => 1);
+        when '-' =>
+            Put (First - Second, Width => 1);
+        when '*' =>
+            Put (First * Second, Width => 1);
+        when '/' =>
+            if second = 0 then raise Constraint_Error	; end if;
+            Put (First / Second, Width => 1);
+        when others =>
+            Put ("Invalid operator '");
+            Put (Operator);
+            Put ("'");
+        end case;
+      New_Line;
+      end Calculator;
