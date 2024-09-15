1534d4d528ce29509d00adc4fcc3bf9048366f27
Julio Toboso <JulioToboso@gmail.com>
2024-09-15 19:05:05 +0200

    Update Generic_Utilities.adb

--- 8.1. Word Lists/Generic_Utilities.adb
+++ 8.1. Word Lists/Generic_Utilities.adb
@@ -9,87 +9,87 @@ package body Generic_Utilities is
 
    -- Display attributes of a floating-point number
    procedure Display_Attributes (Float_Name : in String; Data : in Float_Type) is
-   begin
+      begin
       Ada.Text_IO.Put_Line(Float_Name & " = " & Float_Type'Image(Data));
-   end Display_Attributes;
+      end Display_Attributes;
 
    -- Swap two items
    procedure Swap (Left, Right : in out Item) is
       Temp : Item;
-   begin
+      begin
       Temp := Left;
       Left := Right;
       Right := Temp;
-   end Swap;
+      end Swap;
 
    -- Bubble sort implementation
    procedure Bubble_Sort(In_Data : in Data_Set; Out_Data : out Data_Set) is
-   begin
+      begin
       -- Bubble sort logic here, simplified for readability
       Out_Data := In_Data;
       for I in Out_Data'Range loop
          for J in I + 1 .. Out_Data'Last loop
             if Out_Data(J) > Out_Data(I) then
                Swap(Out_Data(J), Out_Data(I));
-            end if;
+               end if;
+            end loop;
          end loop;
-      end loop;
-   end Bubble_Sort;
+      end Bubble_Sort;
 
    -- Get the next item in an enumerated type or integer set
    function Next(The_Data : in Item) return Item is
-   begin
+      begin
       if The_Data = Item'Last then
          return Item'First;
       else
          return Item'Succ(The_Data);
-      end if;
-   end Next;
+         end if;
+      end Next;
 
    -- Get the previous item in an enumerated type or integer set
    function Prev(The_Data : in Item) return Item is
-   begin
+      begin
       if The_Data = Item'First then
          return Item'Last;
       else
          return Item'Pred(The_Data);
-      end if;
-   end Prev;
+         end if;
+      end Prev;
 
    -- Display a 2D array on the console
    procedure Display_2D_Array_On_Console(The_Array : in TwoD_Array_Type) is
-   begin
+      begin
       for I in The_Array'Range(1) loop
          for J in The_Array'Range(2) loop
             Ada.Text_IO.Put(Item'Image(The_Array(I, J)) & " ");
-         end loop;
+            end loop;
          Ada.Text_IO.New_Line;
-      end loop;
-   end Display_2D_Array_On_Console;
+         end loop;
+      end Display_2D_Array_On_Console;
 
    -- Apply a method to modify each item in a dataset
    procedure Modify_By_Method(The_Data : in out Data_Set) is
       I : Index := The_Data'First;
-   begin
+      begin
       while I /= The_Data'Last loop
          Method(The_Data(I));
          I := Index'Succ(I);
-      end loop;
-   end Modify_By_Method;
+         end loop;
+      end Modify_By_Method;
 
    -- Convert radians to degrees
    function To_Degrees(R : in Radian) return Degree is
       Result : Degree := Degree(180.0 * R) / Numerics.Pi;
-   begin
+      begin
       return Result;
-   end To_Degrees;
+      end To_Degrees;
 
    -- Convert degrees to radians
    function To_Radians(D : in Degree) return Radian is
       Work_1 : Long_Float := (Numerics.Pi * Long_Float(D)) / 180.0;
       Result : Radian := Radian(Work_1);
-   begin
+      begin
       return Result;
-   end To_Radians;
+      end To_Radians;
 
-end Generic_Utilities;
+   end Generic_Utilities;
