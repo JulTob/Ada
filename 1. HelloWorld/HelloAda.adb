8adb00bca7a497b8e3f83edd7ea7432b873c831e
Julio Toboso <JulioToboso@gmail.com>
2025-05-05 10:25:54 +0200

    Update HelloAda.adb

--- 1. HelloWorld/HelloAda.adb
+++ 1. HelloWorld/HelloAda.adb
@@ -1,11 +1,11 @@
 with Ada.Text IO;
 
 procedure HelloAda is 
-begin
-  Ada.Text_IO.Put_Line(”Hello, Ada! ”); 
-  Ada.Text_IO.Put("Such a nice day")
-  Ada.Text_IO.New_Line;
-end HelloAda;
+  begin
+    Ada.Text_IO.Put_Line(”Hello, Ada! ”); 
+    Ada.Text_IO.Put("Such a nice day")
+    Ada.Text_IO.New_Line;
+    end HelloAda;
 
 
 -- Compilation:
